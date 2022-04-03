87d4121adcb5febe794b4154d9e24e4db78a1f85
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-03 13:30:59 +0200

    Retrieve SSM params in stack

--- lib/reference-ssm-params-in-cdk-stack.ts
+++ lib/reference-ssm-params-in-cdk-stack.ts
@@ -1,16 +1,40 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class ReferenceSsmParamsInCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const importedEmail = ssm.StringParameter.fromStringParameterAttributes(
+            this, 'imported-Email', {
+            parameterName: 'email',
+            version: 1
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ReferenceSsmParamsInCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const importedPassword = ssm.StringParameter.fromSecureStringParameterAttributes(
+            this, 'imported-Password', {
+            parameterName: 'db-password',
+            version: 1
+        });
+
+        const importedEnvs = ssm.StringParameter.fromStringParameterAttributes(
+            this, 'imported-Envs', {
+            parameterName: 'environments',
+            version: 1
+        });
+
+        new CfnOutput(this, 'imported-Email-Value', {
+            value: importedEmail.stringValue,
+        });
+
+        new CfnOutput(this, 'imported-Password-Param', {
+            value: importedPassword.parameterName,
+        });
+
+        new CfnOutput(this, 'imported-Envs-Value', {
+            value: importedEnvs.stringValue,
+        });
+
+    }
 }
